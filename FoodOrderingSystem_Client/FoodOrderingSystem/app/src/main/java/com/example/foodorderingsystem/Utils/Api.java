fb1bd4ea971cf3276c89f3f083ab847f367a716d
Lag <hienntqce140311@fpt.edu.vn>
2021-06-17 17:34:40 +0700

    Commit layout Login

--- FoodOrderingSystem_Client/FoodOrderingSystem/app/src/main/java/com/example/foodorderingsystem/Utils/Api.java
+++ FoodOrderingSystem_Client/FoodOrderingSystem/app/src/main/java/com/example/foodorderingsystem/Utils/Api.java
@@ -2,7 +2,7 @@ package com.example.foodorderingsystem.Utils;
 
 public class Api {
     //create string url
-    public static final String URL="http://192.168.1.32:8080/product/";
+    public static final String URL="http://192.168.1.149:8080/product/";
 
     public static ApiInterface getClients(){
         //return element of url
