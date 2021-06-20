025ab6a0d3124b17fe03e10c1231c1e8f4629bca
HuaVinh <quocvinh12360@gmail.com>
2021-06-20 23:09:42 +0700

    create menu main, validation signup, session

---
+++ FoodOrderingSystem_Client/FoodOrderingSystem/app/src/main/java/com/example/foodorderingsystem/fragment/HomeFragment.java
@@ -0,0 +1,31 @@
+package com.example.foodorderingsystem.fragment;
+
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.foodorderingsystem.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment {
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_main, container, false);
+    }
+
+}
